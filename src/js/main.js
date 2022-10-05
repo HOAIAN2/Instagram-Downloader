abb223de6da197e3b188e76d5591e98020023de8
unknown <98139595+HOAIAN2@users.noreply.github.com>
2022-10-05 11:57:29 +0700

    Optimize Performance, Reduce network traffic (x2->x5 loading speed

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,19 @@
-const PROFILE_ID = '51963237586'
 let postShortcode = ''
 let lastShortcode = ''
 const PROFILE_HASH = '69cba40317214236af40e7efa697781d'
 const POST_HASH = '9f8827793ef34641b2fb195d4d41151c'
-const PROFILE_URL = `https://www.instagram.com/graphql/query/?query_hash=${PROFILE_HASH}&variables={"id":"${PROFILE_ID}","first":1}`
 function getShortcode() {
-    const PAGE = ['p', 'reel', 'tv']
+    const DOWNLOADABLE_PAGE = ['p', 'reel', 'tv']
     const currentPage = window.location.pathname
     const pagePath = currentPage.split('/')
-    if (PAGE.includes(pagePath[1])) {
+    if (DOWNLOADABLE_PAGE.includes(pagePath[1])) {
         postShortcode = pagePath[2]
     }
     return postShortcode
 }
-async function fetchFirstPost(PROFILE_URL) {
+async function setDefaultShortcode() {
+    const PROFILE_ID = '51963237586'
+    const PROFILE_URL = `https://www.instagram.com/graphql/query/?query_hash=${PROFILE_HASH}&variables={"id":"${PROFILE_ID}","first":1}`
     const respone = await fetch(PROFILE_URL).catch(error => {
         console.log(error)
         return null
@@ -22,7 +22,7 @@ async function fetchFirstPost(PROFILE_URL) {
     const json = await respone.json()
     postShortcode = json.data.user.edge_owner_to_timeline_media.edges[0].node.shortcode
 }
-async function fetchPostPhotos(postURL) {
+async function getPostPhotos(postURL) {
     const respone = await fetch(postURL).catch(error => {
         console.log(error)
         return null
@@ -31,25 +31,28 @@ async function fetchPostPhotos(postURL) {
     const json = await respone.json()
     return json
 }
-async function fetchPhotos(photoURL) {
-    const respone = await fetch(photoURL).catch(error => {
+async function downloadPhoto(photoURL, fileName) {
+    const respone = await fetch(photoURL.src).catch(error => {
         console.log(error)
         return null
     })
-    if (!respone) return ''
+    if (!respone) return
     const blob = await respone.blob()
     const downloadURL = URL.createObjectURL(blob)
-    return downloadURL
+    const a = document.createElement('a')
+    a.href = downloadURL
+    a.download = fileName
+    a.click()
+    URL.revokeObjectURL(a.href)
 }
-function downloadState(state = 'Ready', PHOTOS_DIV) {
+function downloadState(state = 'Ready', PHOTOS_CONTAINER) {
     const DOWNLOAD_BUTTON = document.querySelector('#Download-Button')
     switch (state) {
         case 'Ready':
             DOWNLOAD_BUTTON.className = 'Downloading'
             DOWNLOAD_BUTTON.textContent = 'Loading...'
             DOWNLOAD_BUTTON.disabled = true
-            PHOTOS_DIV.querySelectorAll('a').forEach(item => {
-                URL.revokeObjectURL(item.href)
+            PHOTOS_CONTAINER.querySelectorAll('img , video').forEach(item => {
                 item.remove()
             })
             break
@@ -68,13 +71,13 @@ function downloadState(state = 'Ready', PHOTOS_DIV) {
 }
 async function downloadPostPhotos() {
     postShortcode = getShortcode()
-    const DISPLAY_DIV = document.querySelector('#Display-Container')
-    const PHOTOS_DIV = document.querySelector('#Photos-Container')
+    const DISPLAY_CONTAINER = document.querySelector('#Display-Container')
+    const PHOTOS_CONTAINER = document.querySelector('#Photos-Container')
     const postURL = `https://www.instagram.com/graphql/query/?query_hash=${POST_HASH}&variables={"shortcode":"${postShortcode}"}`
-    DISPLAY_DIV.className = 'Show'
+    DISPLAY_CONTAINER.className = 'Show'
     if (postShortcode === lastShortcode) return
-    downloadState('Ready', PHOTOS_DIV)
-    const jsonRespone = await fetchPostPhotos(postURL)
+    downloadState('Ready', PHOTOS_CONTAINER)
+    const jsonRespone = await getPostPhotos(postURL)
     if (!jsonRespone) {
         downloadState('Fail')
         return
@@ -84,14 +87,7 @@ async function downloadPostPhotos() {
         const photosArrayLength = photosArray.length
         for (let i = 0; i < photosArrayLength; i++) {
             if (photosArray[i].node.is_video == true) {
-                const anchor =
-                    `<a>
-                        <video>
-                    </a>`
-                const anchorAttributes = {
-                    href: await fetchPhotos(photosArray[i].node.video_url),
-                    download: `${postShortcode}_${i}.mp4`
-                }
+                const video = document.createElement('video')
                 const videoAttributes = {
                     class: 'Photos-Items',
                     id: `${postShortcode}_${i}`,
@@ -99,52 +95,36 @@ async function downloadPostPhotos() {
                     title: `${jsonRespone.data.shortcode_media.owner.full_name} | ${jsonRespone.data.shortcode_media.owner.username} | ${postShortcode}_${i}`,
                     controls: ''
                 }
-                const anchorElement = new DOMParser().parseFromString(anchor, 'text/html').body.firstElementChild
-                Object.keys(anchorAttributes).forEach(key => {
-                    anchorElement.setAttribute(key, anchorAttributes[key])
-                })
                 Object.keys(videoAttributes).forEach(key => {
-                    anchorElement.firstElementChild.setAttribute(key, videoAttributes[key])
+                    video.setAttribute(key, videoAttributes[key])
                 })
-                PHOTOS_DIV.appendChild(anchorElement)
+                PHOTOS_CONTAINER.appendChild(video)
+                video.addEventListener('click', () => {
+                    downloadPhoto(video, `${postShortcode}_${i}.mp4`)
+                })
             }
             else {
-                const anchor =
-                    `<a>
-                        <img>
-                    </a>`
-                const anchorAttributes = {
-                    href: await fetchPhotos(photosArray[i].node.display_url),
-                    download: `${postShortcode}_${i}.jpeg`
-                }
+                const img = document.createElement('img')
                 const photoAttributes = {
                     class: 'Photos-Items',
                     id: `${postShortcode}_${i}`,
                     src: photosArray[i].node.display_url,
                     title: `${jsonRespone.data.shortcode_media.owner.full_name} | ${jsonRespone.data.shortcode_media.owner.username} | ${postShortcode}_${i}`
                 }
-                const anchorElement = new DOMParser().parseFromString(anchor, 'text/html').body.firstElementChild
-                Object.keys(anchorAttributes).forEach(key => {
-                    anchorElement.setAttribute(key, anchorAttributes[key])
-                })
                 Object.keys(photoAttributes).forEach(key => {
-                    anchorElement.firstElementChild.setAttribute(key, photoAttributes[key])
+                    img.setAttribute(key, photoAttributes[key])
                 })
-                PHOTOS_DIV.appendChild(anchorElement)
+                PHOTOS_CONTAINER.appendChild(img)
+                img.addEventListener('click', () => {
+                    downloadPhoto(img, `${postShortcode}_${i}.jpeg`)
+                })
             }
         }
     }
     else {
         const photo = jsonRespone.data.shortcode_media
         if (photo.is_video == true) {
-            const anchor =
-                `<a>
-                    <video>
-                </a>`
-            const anchorAttributes = {
-                href: await fetchPhotos(photo.video_url),
-                download: `${postShortcode}.mp4`
-            }
+            const video = document.createElement('video')
             const videoAttributes = {
                 class: 'Photos-Items',
                 id: `${postShortcode}`,
@@ -152,44 +132,35 @@ async function downloadPostPhotos() {
                 title: `${jsonRespone.data.shortcode_media.owner.full_name} | ${jsonRespone.data.shortcode_media.owner.username} | ${postShortcode}`,
                 controls: ''
             }
-            const anchorElement = new DOMParser().parseFromString(anchor, 'text/html').body.firstElementChild
-            Object.keys(anchorAttributes).forEach(key => {
-                anchorElement.setAttribute(key, anchorAttributes[key])
-            })
             Object.keys(videoAttributes).forEach(key => {
-                anchorElement.firstElementChild.setAttribute(key, videoAttributes[key])
+                video.setAttribute(key, videoAttributes[key])
             })
-            PHOTOS_DIV.appendChild(anchorElement)
+            PHOTOS_CONTAINER.appendChild(video)
+            video.addEventListener('click', () => {
+                downloadPhoto(video, `${postShortcode}.mp4`)
+            })
         }
         else {
-            const anchor =
-                `<a>
-                    <img>
-                </a>`
-            const anchorAttributes = {
-                href: await fetchPhotos(photo.display_url),
-                download: `${postShortcode}.jpeg`
-            }
+            const img = document.createElement('img')
             const photoAttributes = {
                 class: 'Photos-Items',
                 id: `${postShortcode}`,
                 src: photo.display_url,
                 title: `${jsonRespone.data.shortcode_media.owner.full_name} | ${jsonRespone.data.shortcode_media.owner.username} | ${postShortcode}`
             }
-            const anchorElement = new DOMParser().parseFromString(anchor, 'text/html').body.firstElementChild
-            Object.keys(anchorAttributes).forEach(key => {
-                anchorElement.setAttribute(key, anchorAttributes[key])
-            })
             Object.keys(photoAttributes).forEach(key => {
-                anchorElement.firstElementChild.setAttribute(key, photoAttributes[key])
+                img.setAttribute(key, photoAttributes[key])
             })
-            PHOTOS_DIV.appendChild(anchorElement)
+            PHOTOS_CONTAINER.appendChild(img)
+            img.addEventListener('click', () => {
+                downloadPhoto(img, `${postShortcode}.jpeg`)
+            })
         }
     }
     downloadState('Success')
 }
 function initUI() {
-    const DIV =
+    const DISPLAY_CONTAINER =
         `<div class="Hide" id="Display-Container">
             <div id="Title-Container">
                 <span>Photos</span>
@@ -198,9 +169,9 @@ function initUI() {
             <div id="Photos-Container"></div>
         </div>`
     const BUTTON = `<button class="Download" id="Download-Button">Download</button>`
-    const DIV_NODE = new DOMParser().parseFromString(DIV, 'text/html').body.firstElementChild
+    const DISPLAY_NODE = new DOMParser().parseFromString(DISPLAY_CONTAINER, 'text/html').body.firstElementChild
     const BUTTON_NODE = new DOMParser().parseFromString(BUTTON, 'text/html').body.firstElementChild
-    document.body.appendChild(DIV_NODE)
+    document.body.appendChild(DISPLAY_NODE)
     document.body.appendChild(BUTTON_NODE)
 }
 function handleEvents() {
@@ -223,7 +194,7 @@ function handleEvents() {
 }
 function main() {
     initUI()
-    fetchFirstPost(PROFILE_URL)
+    setDefaultShortcode()
     handleEvents()
 }
 main()
