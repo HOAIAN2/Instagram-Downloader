0000b281c9be5dfd12e1007236248e7016334376
HOAIAN2 <phunghieuzw@gmail.com>
2022-11-16 09:00:14 +0700

    Update new feature: Download Hightlights Stories

--- src/js/post.js
+++ src/js/post.js
@@ -1,14 +1,14 @@
 function getShortcode() {
     const DOWNLOADABLE_PAGE = ['p', 'reel', 'tv']
     const currentPage = window.location.pathname.split('/')
-    if (DOWNLOADABLE_PAGE.includes(currentPage[1])) postShortcode = currentPage[2]
-    return postShortcode
+    if (DOWNLOADABLE_PAGE.includes(currentPage[1])) appLog.current.shortcode = currentPage[2]
+    return appLog.current.shortcode
 }
 async function getPostPhotos(postURL) {
     try {
         const respone = await fetch(postURL)
         const json = await respone.json()
-        return json.data.shortcode_media
+        return json.data['shortcode_media']
     } catch (error) {
         console.log(error)
         return null
@@ -22,14 +22,13 @@ async function downloadPostPhotos() {
         },
         media: []
     }
-    postShortcode = getShortcode()
-    const postURL = `https://www.instagram.com/graphql/query/?query_hash=${POST_HASH}&variables=${encodeURIComponent(`{"shortcode":"${postShortcode}"}`)}`
+    const postURL = `https://www.instagram.com/graphql/query/?query_hash=${POST_HASH}&variables=${encodeURIComponent(`{"shortcode":"${appLog.current.shortcode}"}`)}`
     const json = await getPostPhotos(postURL)
     if (!json) return null
     jsonRespone.user.username = json.owner['username']
     jsonRespone.user.fullName = json.owner['full_name']
     if (json.hasOwnProperty('edge_sidecar_to_children')) {
-        const items = json['edge_sidecar_to_children'].edges
+        const items = json['edge_sidecar_to_children']['edges']
         items.forEach((item) => {
             if (item.node['is_video'] === true) {
                 const media = {
--- src/js/story.js
+++ src/js/story.js
@@ -1,14 +1,19 @@
 function getUsername() {
     const DOWNLOADABLE_PAGE = ['stories']
     const currentPage = window.location.pathname.split('/')
-    if (DOWNLOADABLE_PAGE.includes(currentPage[1])) username = currentPage[2]
-    return username
+    if (DOWNLOADABLE_PAGE.includes(currentPage[1]) && currentPage[2] !== 'highlights') appLog.current.username = currentPage[2]
+    return appLog.current.username
+}
+function getHightlightsID() {
+    const currentPage = window.location.pathname.split('/')
+    if (currentPage[1] === 'stories' && currentPage[2] === 'highlights') appLog.current.highlights = currentPage[3]
+    return appLog.current.highlights
 }
 async function getUserID(options) {
     try {
-        const respone = await fetch(`https://www.instagram.com/api/v1/users/web_profile_info/?username=${username}`, options)
+        const respone = await fetch(`https://www.instagram.com/api/v1/users/web_profile_info/?username=${appLog.current.username}`, options)
         const json = await respone.json()
-        return json.data.user.id
+        return json.data.user['id']
     } catch (error) {
         console.log(error)
         return ''
@@ -24,23 +29,33 @@ async function getStoryPhotos(userID = '51963237586', options) {
         return null
     }
 }
-async function downloadStoryPhotos() {
+async function getHighlightStory(storyID, options) {
+    try {
+        const respone = await fetch(`https://www.instagram.com/api/v1/feed/reels_media/?reel_ids=highlight:${storyID}`, options)
+        const json = await respone.json()
+        return json.reels[`highlight:${storyID}`]
+    } catch (error) {
+        console.log(error)
+        return null
+    }
+}
+async function downloadStoryPhotos(type = 1) {
     const csrftoken = document.cookie.split(' ')[2].split('=')[1]
     const claim = sessionStorage.getItem('www-claim-v2')
     const options = {
         headers: {
-            "x-asbd-id": "198387", // Unkown
-            "x-csrftoken": csrftoken, // Cookie "csrftoken" can delete
-            "x-ig-app-id": "936619743392459", // Unkown
-            "x-ig-www-claim": claim, // Section "www-claim-v2"
-            "x-instagram-ajax": "1006598911",
-            "x-requested-with": "XMLHttpRequest"
+            'x-asbd-id': '198387', // Unkown
+            'x-csrftoken': csrftoken, // Cookie "csrftoken" can delete
+            'x-ig-app-id': '936619743392459', // Unkown
+            'x-ig-www-claim': claim, // Section "www-claim-v2"
+            'x-instagram-ajax': '1006598911',
+            'x-requested-with': 'XMLHttpRequest'
         },
-        referrer: "https://www.instagram.com/?theme=dark",
-        referrerPolicy: "strict-origin-when-cross-origin",
-        method: "GET",
-        mode: "cors",
-        credentials: "include"
+        referrer: 'https://www.instagram.com',
+        referrerPolicy: 'strict-origin-when-cross-origin',
+        method: 'GET',
+        mode: 'cors',
+        credentials: 'include'
     }
     const jsonRespone = {
         user: {
@@ -49,15 +64,21 @@ async function downloadStoryPhotos() {
         },
         media: []
     }
-    const userID = await getUserID(options)
-    const json = await getStoryPhotos(userID, options)
+    let json = null
+    if (type === 2) {
+        json = await getHighlightStory(appLog.current.highlights, options)
+    }
+    else {
+        const userID = await getUserID(options)
+        json = await getStoryPhotos(userID, options)
+    }
     if (!json) return null
-    jsonRespone.user.username = username
-    jsonRespone.user.fullName = json.user["full_name"]
+    jsonRespone.user.username = json.user['username']
+    jsonRespone.user.fullName = json.user['full_name']
     json.items.forEach((item) => {
         if (item['media_type'] === 1) {
             const media = {
-                url: item['image_versions2'].candidates[0].url,
+                url: item['image_versions2'].candidates[0]['url'],
                 isVideo: false
             }
             jsonRespone.media.push(media)
