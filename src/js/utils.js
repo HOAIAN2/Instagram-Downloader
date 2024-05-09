a9d57098afa09f5432bc3c67f4f11b09bf98245d
Lê Hoài Ân <98139595+HOAIAN2@users.noreply.github.com>
2024-05-09 12:18:28 +0700

    Move some functions to utils

---
+++ src/js/utils.js
@@ -0,0 +1,56 @@
+function isValidJson(string) {
+	try {
+		JSON.parse(string)
+		return true
+	} catch {
+		return false
+	}
+}
+
+function saveFile(blob, fileName) {
+	const a = document.createElement('a')
+	a.download = fileName
+	a.href = URL.createObjectURL(blob)
+	a.click()
+	URL.revokeObjectURL(a.href)
+}
+
+function getAuthOptions() {
+	const csrftoken = document.cookie.split(' ')[2].split('=')[1]
+	const claim = sessionStorage.getItem('www-claim-v2')
+	const options = {
+		headers: {
+			'x-asbd-id': '129477',
+			'x-csrftoken': csrftoken,
+			'x-ig-app-id': APP_ID,
+			'x-ig-www-claim': claim,
+			'x-instagram-ajax': '1006598911',
+			'x-requested-with': 'XMLHttpRequest'
+		},
+		referrer: 'https://www.instagram.com',
+		referrerPolicy: 'strict-origin-when-cross-origin',
+		method: 'GET',
+		mode: 'cors',
+		credentials: 'include'
+	}
+	return options
+}
+
+function findKeyInNestedObject(obj, key) {
+	if (typeof obj !== 'object' || obj === null) return null
+	if (Array.isArray(obj)) {
+		for (const element of obj) {
+			const result = findKeyInNestedObject(element, key)
+			if (result !== null) return result
+		}
+		return null
+	}
+	if (obj.hasOwnProperty(key)) return obj[key]
+	for (const prop in obj) {
+		if (obj.hasOwnProperty(prop)) {
+			const result = findKeyInNestedObject(obj[prop], key)
+			if (result !== null) return result
+		}
+	}
+	return null
+}
