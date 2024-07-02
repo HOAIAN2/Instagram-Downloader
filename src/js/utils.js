d40de8829929c3d0b34c32eec9c967c56b3a39b3
Lê Hoài Ân <98139595+HOAIAN2@users.noreply.github.com>
2024-07-02 13:35:49 +0700

    Remove JSZip, use Uint8Array to zip file

--- src/js/utils.js
+++ src/js/utils.js
@@ -107,53 +107,6 @@ async function saveZip() {
 	}
 }
 
-// async function saveZip() {
-// 	const DOWNLOAD_BUTTON = document.querySelector('.download-button');
-// 	DOWNLOAD_BUTTON.classList.add('loading');
-// 	DOWNLOAD_BUTTON.textContent = 'Loading...';
-// 	DOWNLOAD_BUTTON.disabled = true;
-// 	let count = 0;
-// 	const zip = new JSZip();
-// 	const medias = Array.from(document.querySelectorAll('.overlay.checked')).map(item => item.previousElementSibling);
-// 	const zipFileName = medias[0].title.split(' | ').slice(1, 5).join('_') + '.zip';
-// 	async function fetchSelectedMedias() {
-// 		const results = await Promise.allSettled(medias.map(async (media) => {
-// 			const res = await fetch(media.src);
-// 			const blob = await res.blob();
-// 			const data = {
-// 				title: media.title.split(' | ').slice(1, 5).join('_'),
-// 				data: blob
-// 			};
-// 			if (media.nodeName === 'VIDEO') data.title = `${data.title}.mp4`;
-// 			else data.title = `${data.title}.jpeg`;
-// 			count++;
-// 			DOWNLOAD_BUTTON.textContent = `${count}/${medias.length}`;
-// 			return data;
-// 		}));
-// 		results.forEach(promise => {
-// 			if (promise.status === 'rejected') throw new Error('Fail to fetch');
-// 			zip.file(promise.value.title, promise.value.data, { base64: true });
-// 		});
-// 	}
-// 	try {
-// 		await fetchSelectedMedias();
-// 		console.time();
-// 		const blob = await zip.generateAsync({ type: 'blob' }, ((metadata) => {
-// 			DOWNLOAD_BUTTON.textContent = `${Math.floor(metadata.percent)} %`;
-// 		}));
-// 		saveFile(blob, zipFileName);
-// 		document.querySelectorAll('.overlay.checked').forEach(element => {
-// 			element.classList.remove('checked');
-// 			element.classList.add('saved');
-// 		});
-// 		resetDownloadState();
-// 	} catch (error) {
-// 		console.log(error);
-// 		resetDownloadState();
-// 	}
-// 	console.timeEnd();
-// }
-
 function shouldDownload() {
 	appState.setCurrentShortcode();
 	appState.setCurrentUsername();
