ccd595ca1dfd33a0cba765182991e8e66777e539
Lê Hoài Ân <98139595+HOAIAN2@users.noreply.github.com>
2024-07-07 13:45:53 +0700

    Clean code

--- src/js/global/event.js
+++ src/js/global/event.js
@@ -3,14 +3,13 @@
 	const observer = new MutationObserver(() => {
 		const newPath = window.location.pathname;
 		if (newPath !== path) {
-			const pathChangeEvent = new CustomEvent('pathChange', {
+			path = newPath;
+			window.dispatchEvent(new CustomEvent('pathChange', {
 				detail: {
 					oldPath: path,
 					currentPath: newPath
 				}
-			});
-			path = newPath;
-			window.dispatchEvent(pathChangeEvent);
+			}));
 		}
 	});
 	observer.observe(document.body, {
