48f0658fc72be1fefbffcb31af772e6e24eeb3fe
Lê Hoài Ân <98139595+HOAIAN2@users.noreply.github.com>
2024-12-31 10:51:11 +0700

    catch error on findValueByKey

--- src/js/global/utils.js
+++ src/js/global/utils.js
@@ -7,7 +7,12 @@ const findValueByKey = (obj, key) => {
         const current = stack.pop();
         if (visited.has(current)) continue;
         visited.add(current);
-        if (current[key] !== undefined) return current[key];
+        try {
+            if (current[key] !== undefined) return current[key];
+        } catch (error) {
+            if (error.name === 'SecurityError') continue;
+            console.log(error);
+        }
         for (const value of Object.values(current)) {
             if (typeof value === 'object' && value !== null) {
                 stack.push(value);
