f106eff69accdd77a83f98c20fa3a56bec8fdab4
Lê Hoài Ân <98139595+HOAIAN2@users.noreply.github.com>
2025-12-26 10:07:00 +0700

    fix not fetch correct story after watch story from home.

--- src/js/global/stories-view-handler.js
+++ src/js/global/stories-view-handler.js
@@ -3,12 +3,15 @@ window.addEventListener('pathChange', (e) => {
         const section = Array.from(document.querySelectorAll('section')).pop();
         const username = getValueByKey(section, 'username');
         const userId = getValueByKey(section, 'userId');
-        if (username && userId)
-            window.dispatchEvent(new CustomEvent('userLoad', {
-                detail: {
-                    username: username,
-                    id: userId
-                }
-            }));
+        /**
+         * Temporary disable due to Instagram breaking change view story from home.
+         */
+        // if (username && userId)
+        //     window.dispatchEvent(new CustomEvent('userLoad', {
+        //         detail: {
+        //             username: username,
+        //             id: userId
+        //         }
+        //     }));
     }
 });
