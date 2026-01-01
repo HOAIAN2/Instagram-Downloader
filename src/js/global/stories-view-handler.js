b025a536e05f9389a3825299faa5b8ab672d973b
Lê Hoài Ân <98139595+HOAIAN2@users.noreply.github.com>
2026-01-01 12:45:20 +0700

    update stories-view-handler.js

--- src/js/global/stories-view-handler.js
+++ src/js/global/stories-view-handler.js
@@ -1,17 +1,21 @@
 window.addEventListener('pathChange', (e) => {
     if (e.detail.currentPath.match(/\/(stories)\/(.*?)\/(\d*)(\/?)/)) {
-        const section = Array.from(document.querySelectorAll('section')).pop();
-        const username = getValueByKey(section, 'username');
-        const userId = getValueByKey(section, 'userId');
         /**
-         * Temporary disable due to Instagram breaking change view story from home.
+         * Why setTimeout needed?
+         * Because Instagram will load ads before stories and make userId and username mismatch
+         * So you need to "wait" the stories to fully load to get correct username and userId
          */
-        // if (username && userId)
-        //     window.dispatchEvent(new CustomEvent('userLoad', {
-        //         detail: {
-        //             username: username,
-        //             id: userId
-        //         }
-        //     }));
+        setTimeout(() => {
+            const section = Array.from(document.querySelectorAll('section')).pop();
+            const username = getValueByKey(section, 'username');
+            const userId = getValueByKey(section, 'userId');
+            if (username && userId)
+                window.dispatchEvent(new CustomEvent('userLoad', {
+                    detail: {
+                        username: username,
+                        id: userId
+                    }
+                }));
+        }, 0);
     }
 });
